5e82d2d7c0993b6a72d26e90b56955cf8ccad7ff
Kushagra Singh <kushagrasingh543@gmail.com>
2023-06-01 21:24:02 +0530

    added JS-Engine

--- src/js/main.js
+++ src/js/main.js
@@ -3,16 +3,23 @@ const liftContainer = document.getElementById("section-2");
 const floorData = document.getElementById("floorData");
 const liftData = document.getElementById("liftData");
 const createButton = document.getElementById("createUI");
+const waiting = document.getElementById("waiting");
 // const lift = document.getElementById("mainLift");
 let gate1;
 let gate2;
 let totalFloor;
 let lift;
+// lift2;
+
+/////////////////// USER INPUT FIELD AND UI CREATION///////////////////
+
 createButton.addEventListener("click", (event) => {
   event.preventDefault();
   let currentFloor = 0;
   let nextFloor;
   let final;
+  let prevTouchDown;
+  let finalTouchDown;
 
   totalFloor = floorData.value;
   totalLift = liftData.value;
@@ -24,12 +31,67 @@ createButton.addEventListener("click", (event) => {
   </div>
     `;
     if (liftContainer) liftContainer.insertAdjacentHTML("afterbegin", html);
+
     lift = document.getElementById(`mainLift-${i + 1}`);
+    // : (lift2 = document.getElementById(`mainLift-${i + 1}`));
     console.log(lift);
     gate1 = document.getElementById("gate-1");
     gate2 = document.getElementById("gate-2");
   }
 
+  /////////////////////LIFT MOVEMENT AND GATE OPEN CLOSE////////////////////////
+
+  let onwork = false;
+  let actionQueue = [];
+  const openGate = (nextFloor, final) => {
+    return new Promise((resolve, reject) => {
+      lift.style.bottom = `${nextFloor * 21}vh`;
+      lift.style.transition = `all ${final * 2}s`;
+      setTimeout(() => {
+        gate1.style.left = "92px";
+        gate2.style.right = "92px";
+
+        // closeGate();
+        setTimeout(() => {
+          gate1.style.left = "44px";
+          gate2.style.right = "44px";
+          resolve();
+        }, 2500);
+      }, 2000 * final);
+    });
+  };
+
+  const closeGate = (final) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        waiting.style.display = "none";
+        // downButton.textContent = "Down";
+        // gate1.style.left = "44px";
+        // gate2.style.right = "44px";
+        resolve();
+      }, 2500);
+    });
+  };
+
+  const processActionQueue = async () => {
+    console.log("before:", actionQueue);
+
+    onwork = true;
+    while (actionQueue.length > 0) {
+      const { nextFloor, final } = actionQueue.shift();
+      console.log(actionQueue);
+      try {
+        await openGate(nextFloor, final);
+        await closeGate();
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    onwork = false;
+  };
+
+  /////////////////////////// FLOOOR CREATION AND LIFT BUTTON//////////////////////////
   for (let i = 0; i <= totalFloor - 1; i++) {
     let button;
     if (i === 0) {
@@ -43,7 +105,7 @@ createButton.addEventListener("click", (event) => {
 
     let html = `
   <div class="floor">
-  <div>floor${i + 1}</div>
+  <div class="floorNum">F-${i + 1}</div>
         <div class="actions">
         ${button}
 
@@ -54,21 +116,28 @@ createButton.addEventListener("click", (event) => {
 
     if (floorContainer) floorContainer.insertAdjacentHTML("afterbegin", html);
     if (i < totalFloor - 1) {
+      nextFloor = i;
+      final = Math.abs(currentFloor - nextFloor);
       let upButton = document.getElementById(`upButton-${i}`);
+
       upButton.addEventListener("click", () => {
+        // upButton.style.backgroundColor = "red";
         nextFloor = i;
         final = Math.abs(currentFloor - nextFloor);
 
-        lift.style.bottom = `${i * 21}vh`;
-        lift.style.transition = `all ${2 * final}s`;
-        setTimeout(() => {
-          gate1.style.left = "92px";
-          gate2.style.right = "92px";
-          setTimeout(() => {
-            gate1.style.left = "44px";
-            gate2.style.right = "44px";
-          }, 2500);
-        }, 2000 * final);
+        if (onwork) {
+          actionQueue.push({ nextFloor, final });
+          currentFloor = nextFloor;
+          waiting.style.display = "block";
+          console.log("The lift is busy. Please wait.");
+          return;
+        }
+
+        actionQueue.push({ nextFloor, final });
+
+        if (!onwork) {
+          processActionQueue();
+        }
         currentFloor = nextFloor;
       });
     }
@@ -77,22 +146,29 @@ createButton.addEventListener("click", (event) => {
       let downButton = document.getElementById(`downButton-${i}`);
 
       downButton.addEventListener("click", () => {
+        // downButton.style.backgroundColor = "red";
         nextFloor = i;
         final = Math.abs(currentFloor - nextFloor);
 
-        lift.style.bottom = `${i * 21}vh`;
-        lift.style.transition = `all ${final * 2}s`;
-        setTimeout(() => {
-          gate1.style.left = "92px";
-          gate2.style.right = "92px";
-          setTimeout(() => {
-            gate1.style.left = "44px";
-            gate2.style.right = "44px";
-          }, 2500);
-        }, 2000 * final);
+        if (onwork) {
+          actionQueue.push({ nextFloor, final });
+          currentFloor = nextFloor;
+          waiting.style.display = "block";
+          downButton.textContent = "Inline...";
+          return;
+        }
+
+        actionQueue.push({ nextFloor, final });
+
+        if (!onwork) {
+          processActionQueue();
+
+          // downButton.style.backgroundColor = "blue";
+        }
         currentFloor = nextFloor;
       });
     }
   }
   lift.style.display = "flex";
+  // lift2.style.display = "flex";
 });
